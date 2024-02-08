47eef3d8a89d7012cdee7c363bd8276ab1692dc2
Kotaro Tanaka <kotaro.tanaka@rakus-partners.co.jp>
2024-02-08 13:48:09 +0900

    ex05 commit

---
+++ src/main/java/copilot/exercises/ex05/p02/Guitar.java
@@ -0,0 +1,15 @@
+package copilot.exercises.ex05.p02;
+
+//インターフェースの実装を行います
+public class Guitar {
+//     GuitarクラスではadjustSoundメソッドをオーバーライドして特定の動作を追加し
+// }   GuitarクラスのadjustSoundメソッドは、"Guitar sound is adjusted."と表示します
+// - GuitarクラスのplaySoundメソッドは、"Guitar sound is played."と表示します
+    public void adjustSound() {
+        System.out.println("Guitar sound is adjusted.");
+    }
+
+    public void playSound() {
+        System.out.println("Guitar sound is played.");
+    }
+}
